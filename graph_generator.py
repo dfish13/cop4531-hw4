ccdddee5574c1be9035a5fdbcfa0ad85a513fc9f
Duncan Fisher <fisherduncan13@yahoo.com>
2017-04-22 23:58:36 -0400

    submission

--- graph_generator.py
+++ graph_generator.py
@@ -1,7 +1,12 @@
 import random
+import numpy as np
 
 MAX_LIGHT_YEARS = 1
 EDGE_RANGE = (3, 8)
+
+array = [1]
+for i in range(99):
+	array = [0] + array
 
 n = 0
 s = []
@@ -10,12 +15,23 @@ for star in starfile:
 	s = [star.strip()] + s
 	n += 1
 
+m = np.zeros((n, n))
+for i in range(n):
+	for j in range(i+1,n):
+		if random.choice(array):
+			c = round(random.random()*MAX_LIGHT_YEARS, 3)
+		else:
+			c = 0
+		m[i][j] = c
+		m[j][i] = c
+
 print(n)
 for i in range(n):
 	print(s[i], end='')
-	for j in range(0, random.randint(*EDGE_RANGE)):
-		print(' ', end='')
-		print(s[random.randint(0, n-1)], end=' ')
-		print(round(random.random()*MAX_LIGHT_YEARS, 3), end='')
+	for j in range(n):
+		if m[i][j] != 0:
+			print(' ', end='')
+			print(s[j], end=' ')
+			print(m[i][j], end='')
 	print()
 	
